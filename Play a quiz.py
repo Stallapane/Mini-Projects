dd681489f9667398d25d90d87b9a12e5bfd4d74b
Sravani Tallapaneni <134858342+Stallapane@users.noreply.github.com>
2024-05-16 23:31:31 -0400

    Update Play a quiz.py

--- Play a quiz.py
+++ Play a quiz.py
@@ -1,41 +1,16 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 
 print("Hello! Welcome to the Quiz")
 
-
-# In[2]:
-
-
 play = input("Do you want to play? ")
-
-
-# In[5]:
-
 
 if play.lower() != 'yes':
     quit()
 else:
     print('Lets get started')
 
-
-# In[10]:
-
-
 score = 0
 
-
-# In[11]:
-
-
 answer = input("what is the band that never plays music? ")
-
-
-# In[12]:
-
 
 if answer.lower() == "rubber band":
     print("it's correct")
@@ -44,13 +19,7 @@ else:
     print("oops you are wrong :)")
 
 
-# In[13]:
-
-
 answer = input("Most tensed animal? ")
-
-
-# In[14]:
 
 
 if answer.lower() == 'kangaroo':
@@ -59,14 +28,7 @@ if answer.lower() == 'kangaroo':
 else:
     print("oh no! Incorrect answer")
 
-
-# In[15]:
-
-
 answer =  input("who is a short mom? ")
-
-
-# In[17]:
 
 
 if answer.lower() == 'minimum':
@@ -76,14 +38,7 @@ else:
     print("Incorrect")
 
 
-# In[18]:
-
-
 answer = input("what kind of shoes spy wears? ")
-
-
-# In[20]:
-
 
 if answer.lower() == 'sneakers':
     print("correct")
@@ -91,14 +46,7 @@ if answer.lower() == 'sneakers':
 else: 
     print("Better luck next time")
 
-
-# In[24]:
-
-
 print(f"Your Score is {score}.")
-
-
-# In[ ]:
 
 
 
